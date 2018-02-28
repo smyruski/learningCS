ac13a61c2dd7618ee6c61efe96b9c31f4159f5e1
smyruski <36239478+smyruski@users.noreply.github.com>
2018-02-28 10:03:21 -0500

    Add files via upload

---
+++ Lesson 3.py
@@ -0,0 +1,71 @@
+#print("Hello World")
+
+#print("Hi Hyein!")
+
+#print(1)
+
+
+#print("Chocolate is the best thing in the world")
+
+
+
+
+#your_name = "Sarah"
+#print(your_name)
+
+#your_name = raw_input("What is your name?")
+
+#print "Welcome to Computer Science", your_name
+
+#siblings = raw_input("How many siblings do you have?")
+#names = raw_input("What are their names?")
+
+#print "You have", siblings, "sibling(s) named", names
+
+
+
+
+#day = raw_input("What day of the week is it?")
+#print "Today is", day
+
+# January = 145.5
+# February = 88
+# March = 210.07
+#
+# sum_var = January + February + March
+# print sum_var
+# mean_var = (January + February + March)/3
+# print mean_var
+
+#var1 = "Sarah"
+#var2 = 10
+#var3 = 10.000
+#print(type(var1))
+#print(type(var2))
+#print(type(var3))
+
+#str(var2)
+#print var2
+
+
+#sleep = raw_input("How many hours of sleep did you get?")
+#coffee = raw_input("How many cups of coffee did you drink?")
+#print "You had", sleep, "hours of sleep last night and", coffee, "cups of coffee"
+
+
+# varA = int(raw_input("How many hours of sleep did you get Monday?"))
+# varB = int(raw_input("How many hours of sleep did you get Tuesday?"))
+# varC = int(raw_input("How many hours of sleep did you get Wednesday?"))
+#
+# sum_var = varA + varB + varC
+# print "Sum =", sum_var
+# mean_var = (varA + varB + varC)/3
+# print "Mean =", mean_var
+
+
+x = 684
+if x%2 == 0:
+    print "Fizz!"
+
+else:
+    print "Buzz!"

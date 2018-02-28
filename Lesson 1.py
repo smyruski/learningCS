a7e88188f510d883100cd5a2e2bdc841f3139a3e
smyruski <36239478+smyruski@users.noreply.github.com>
2018-02-28 09:20:20 -0500

    CS Classes 1 and 2

---
+++ Lesson 1.py
@@ -0,0 +1,48 @@
+#print("Hello World")
+
+#print("Hi Hyein!")
+
+#print(1)
+
+
+#print("Chocolate is the best thing in the world")
+
+
+
+
+#your_name = "Sarah"
+#print(your_name)
+
+#your_name = raw_input("What is your name?")
+
+#print "Welcome to Computer Science", your_name
+
+#siblings = raw_input("How many siblings do you have?")
+#names = raw_input("What are their names?")
+
+#print "You have", siblings, "sibling(s) named", names
+
+
+
+
+#day = raw_input("What day of the week is it?")
+#print "Today is", day
+
+# January = 145.5
+# February = 88
+# March = 210.07
+#
+# sum_var = January + February + March
+# print sum_var
+# mean_var = (January + February + March)/3
+# print mean_var
+
+var1 = "Sarah"
+var2 = 10
+var3 = 10.000
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+str(var2)
+print var2
